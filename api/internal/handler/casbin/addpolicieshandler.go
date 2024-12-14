838c502a32e37ec515332f7b6bfc8762d02a850d
丁元广 <dinyuanguang@163.com>
2024-12-14 20:51:19 +0800

    refactor: nothing import

--- api/internal/handler/casbin/addpolicieshandler.go
+++ api/internal/handler/casbin/addpolicieshandler.go
@@ -1,12 +1,14 @@
 package casbin
 
 import (
-	"github.com/bearllflee/scholar-track/pkg/response"
 	"net/http"
+
+	"github.com/bearllflee/scholar-track/pkg/response"
 
 	"github.com/bearllflee/scholar-track/api/internal/logic/casbin"
 	"github.com/bearllflee/scholar-track/api/internal/svc"
 	"github.com/bearllflee/scholar-track/api/internal/types"
+	"github.com/zeromicro/go-zero/core/logx"
 	"github.com/zeromicro/go-zero/rest/httpx"
 )
 
@@ -14,6 +16,7 @@ func AddPoliciesHandler(svcCtx *svc.ServiceContext) http.HandlerFunc {
 	return func(w http.ResponseWriter, r *http.Request) {
 		var req types.AddPolicyReq
 		if err := httpx.Parse(r, &req); err != nil {
+			logx.Error("参数错误: ", err)
 			response.ErrWithMessage(r.Context(), w, "参数错误")
 			return
 		}
